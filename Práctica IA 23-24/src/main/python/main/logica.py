17fda3ef0ee72886721d14e19b6d31bbecf2a543
100495754 <100495754@alumnos.uc3m.es>
2024-05-03 17:30:58 +0200

    Creo clase InputVars para simplificar

--- Práctica IA 23-24/src/main/python/main/logica.py
+++ Práctica IA 23-24/src/main/python/main/logica.py
@@ -10,77 +10,15 @@ from main.variables.Assets import Assets
 from main.variables.History import History
 from main.variables.IncomeLevel import IncomeLevel
 from main.variables.Job import Job
+from main.metodos_clases.InputVars import InputVars
 
-app = readApplicationsFile()
-j = 0
 json_files_path = (str(Path.home()) +
-                                   "/PycharmProjects/Practica-IA-2024-G-11/Práctica IA 23-24/src/main/python/main/variables/json")
+                           "/PycharmProjects/Practica-IA-2024-G-11/Práctica IA 23-24/src/main/python/main/variables/json")
 file = json_files_path + '/json_app.json'
-if os.path.isfile(file):
-    os.remove(file)
-try:
-    with open(file, 'r') as f:
-        z = json.load(f)
-except FileNotFoundError:
-    print("File not found. Creating a new file.")
-    z = {}
-    with open(file, 'w') as f:
-        json.dump(z, f)  # Crea un archivo JSON vacío, o ajusta esto según lo que necesites
 
-dict = {}
-for i in app:
-    temp_dict = {}
+input = InputVars()
+input.borrado_y_creado_json()
+input_dict = input.calcular_variables()
+input.guardado_vars(input_dict)
 
-    """Edad"""
-    data = {}
-    edad = app[j].data[0][1]
-    algo = Age(edad)
-    data["Young"],data["Adult"],data["Elder"] = algo.age()
-    temp_dict["Age"] = data
-
-    """IncomeLevel"""
-    data = {}
-    incomelevel = app[j].data[1][1]
-    algo = IncomeLevel(incomelevel)
-    data["Low"], data["Med"], data["Hig"] = algo.incomelevel()
-    temp_dict["IncomeLevel"] = data
-
-    """Assets"""
-    data = {}
-    assets = app[j].data[2][1]
-    algo = Assets(assets)
-    data["Scarce"], data["Moderate"], data["Abundant"] = algo.asset()
-    temp_dict["Assets"] = data
-
-    """Amount"""
-    data = {}
-    amount = app[j].data[3][1]
-    algo = Amount(amount)
-    data["Small"], data["Medium"], data["Big"], data["VeryBig"]= algo.amount()
-    temp_dict["Amount"] = data
-
-    """Job"""
-    data = {}
-    job = app[j].data[4][1]
-    algo = Job(job)
-    data["Unstable"], data["Stable"] = algo.job()
-    temp_dict["Job"] = data
-
-    """History"""
-    data = {}
-    history = app[j].data[5][1]
-    algo = History(history)
-    data["Poor"], data["Standard"], data["Good"] = algo.history()
-    temp_dict["History"] = data
-
-    dict[app[j].appId] = temp_dict
-
-    j += 1
-
-try:
-    with open(file, 'w') as f:
-        # Guarda el diccionario completo en el archivo JSON, con una indentación de 4 espacios
-        json.dump(dict, f, indent=4)
-except FileNotFoundError:
-    print("File not found")
 
